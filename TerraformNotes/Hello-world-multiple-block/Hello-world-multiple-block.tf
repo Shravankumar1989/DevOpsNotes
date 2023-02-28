233c7e30ea433de427ca3a47a688e2e45947dede
shravansahan <shravansahan@gmail.com>
2023-02-28 12:29:28 +0530

    I have created mutltiple block file 2

---
+++ TerraformNotes/Hello-world-multiple-block/Hello-world-multiple-block.tf
@@ -0,0 +1,11 @@
+output "thisIsFirstBlock" {
+	value = "this is first block"
+}
+
+output "thisIsSecondBlock" {
+	value = "this is second block"
+}
+
+output "thisIsthirdBlock" {
+	value = "this is third block"
+}
